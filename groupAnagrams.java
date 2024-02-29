1e3a63690832e317888ad62fec1cbdbbbc1f690e
Dobro2910 <90589894+Dobro2910@users.noreply.github.com>
2024-02-29 20:49:51 +1100

    new problem solve

---
+++ groupAnagrams.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Arrays;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> groupAnagrams = new ArrayList<>();
+        HashMap<String, List<String>> anagramMap = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if (!anagramMap.containsKey(sortedStr)) {
+                anagramMap.put(sortedStr, new ArrayList<>());
+            }
+            anagramMap.get(sortedStr).add(str);
+        }
+
+        groupAnagrams.addAll(anagramMap.values());
+
+        return groupAnagrams;
+    }
+}
